3e3e18eb03e534d2f49a207939d934d2d91cb7bc
David Mascall <github@mascall.net>
2020-06-18 10:40:56 +0100

    After Adding Controllers

---
+++ Hacking-with-macOS/AppKit/Project 1/Project 1/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Project 1
+//
+//  Created by David Mascall on 18/06/2020.
+//  Copyright © 2020 David Mascall. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
