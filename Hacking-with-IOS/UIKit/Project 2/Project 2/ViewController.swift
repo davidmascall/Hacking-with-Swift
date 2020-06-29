a5de5eb01dceea2578e62bb0043db720a61dc5f7
David Mascall <github@mascall.net>
2020-06-29 16:49:57 +0100

    IOS Project 2 FInal

--- Hacking-with-IOS/UIKit/Project 2/Project 2/ViewController.swift
+++ Hacking-with-IOS/UIKit/Project 2/Project 2/ViewController.swift
@@ -12,39 +12,41 @@ class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
-    
+
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
-    
- 
+    var attempts = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
-        
+
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
-        
-        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco",
+                      "nigeria", "poland", "russia", "spain", "uk", "us"]
         askQuestion()
     }
-        
+
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
-        
+
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
+
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+
+        title = countries[correctAnswer].uppercased()+" Your Score: \(score) Attempts: \(attempts)"
     }
-    
+
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
 
@@ -52,14 +54,25 @@ class ViewController: UIViewController {
         title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong! Thats The Flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
-        
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
-    }
-    
-}
 
+        attempts += 1
+
+        if attempts < 10 {
+
+            let alc = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            alc.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(alc, animated: true)
+        } else {
+            let alc = UIAlertController(title: title, message: "Your final score is \(score).", preferredStyle: .alert)
+            alc.addAction(UIAlertAction(title: "End", style: .default, handler: askQuestion))
+            present(alc, animated: true)
+
+            attempts = 0
+            score = 0
+            }
+
+        }
+}
